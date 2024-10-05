0bd079460b17a75ca6738464bfc94a43119a2f36
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2024-10-06 05:12:23 +0530

    the code works when the global.css is changed

--- portfolio/src/app/home/page.jsx
+++ portfolio/src/app/home/page.jsx
@@ -1,73 +1,132 @@
-"use client";
-import React from "react";
-import RightProjectSection from "@/components/v1/RightProjectSection";
-import UpperLeftSection from "@/components/v1/UpperLeftSection";
-import SocialsSection from "@/components/v1/SocialsSection";
-import Image from "next/image";
-import { useMediaQuery } from "react-responsive";
-import MobileImage from "@/assets/ecell2.jpg";
-import tableImage from "@/assets/ecell2.jpg";
-import BottomLeftSection from "@/components/v1/BottomLeftSection";
-import BottomRightSection from "@/components/v1/BottomRightSection";
+// components/MyComponent.js
 
-const Page = () => {
-  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
-  const isTablet = useMediaQuery({
-    query: "(min-width: 641px) and (max-width: 1024px)",
-  });
+import React from 'react';
+import Image from 'next/image';
+import Head from 'next/head';
+import Link from 'next/link';
+import './style.css'; // Import your CSS file here
 
-  // Set image path dynamically based on screen size
-  const imageSrc = isMobile ? MobileImage : tableImage;
-
+const MyComponent = () => {
   return (
-    <div className="min-h-screen h-screen bg-[#151515] text-white p-4 overflow-hidden">
-      <div className="grid grid-cols-12 grid-rows-8 gap-4 h-full">
-        {/* Top Navbar */}
-        <div className="col-span-12 row-span-1 flex items-center bg-gridBlocksBackground p-4 rounded-lg shadow-md">
-          <h1 className="text-2xl font-breathney md:text-4xl font-bold text-[#9B8D78]">
-            Rishabh Dubey
-          </h1>
+    <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Rishabh Dubey</title>
+        <link rel="icon" href="/images/logo_circle.png" type="image/png" />
+        {/* Google Fonts */}
+        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:wght@400..900&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+      </Head>
+      <nav>
+        <ul className="nav-bar">
+          <li className="logo">
+            <Link href="#">Rishabh<span>Dubey</span></Link>
+          </li>
+          <input type="checkbox" id="check" />
+          <span className="menu">
+            <li><Link href="#">About</Link></li>
+            <li><Link href="#">Projects</Link></li>
+            <li><Link href="#">Contact</Link></li>
+            <label htmlFor="check" className="close-menu"><i className="fas fa-times"></i></label>
+          </span>
+          <label htmlFor="check" className="open-menu"><i className="fas fa-bars"></i></label>
+        </ul>
+      </nav>
+
+      <div className="container">
+        <div className="item item1">
+          <Image src="/images/circle_two.png" alt="circle image here" width={500} height={500} />
+          <p className="main_slogan">
+            AWS Certified <span> Cloud </span>Practitioner and
+            <span>web </span> developer, crafting scalable web solutions that
+            optimize performance and user experience.
+          </p>
         </div>
 
-        {/* Main Content: Left & Image Section */}
-        <div className="grid grid-cols-8 col-span-8 row-span-4 gap-4">
-          {/* Left Section */}
-          <div className="col-span-5 bg-gridBlocksBackground p-4 rounded-lg shadow-md">
-            <UpperLeftSection />
-          </div>
-
-          {/* Image Section */}
-          <div className="col-span-3 bg-gridBlocksBackground rounded-lg shadow-lg transition-transform transform hover:scale-105 h-full">
-            <div className="relative w-full h-full">
-              <Image
-                src={imageSrc}
-                alt="Rishabh Dubey"
-                className="rounded-lg object-cover object-center"
-                layout="fill"
-                sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, (max-width: 1024px) 33vw, 25vw"
-              />
+        <div className="container-card item item2">
+          <div className="card" id="jokeCard">
+            <div className="card-inner">
+              <div className="card-front">
+                <p id="jokeQuestion">Click to reveal the joke!</p>
+                <p id="reveal" className="phone-block">Hover/Tap to reveal</p>
+                <p id="reveal-not-phone" className="not-phone">Hover/Tap to reveal</p>
+              </div>
+              <div className="card-back">
+                <p id="jokeAnswer"></p>
+              </div>
             </div>
           </div>
         </div>
 
-        {/* Right Section */}
-        <div className="col-span-4 row-span-6 bg-gridBlocksBackground p-4 rounded-lg shadow-md">
-          <RightProjectSection />
+        <div className="item item3">
+          <p className="title">Projects</p>
+          <Link href="your-target-page.html" className="arrow-btn">
+            <Image className="arrow" src="/images/arrow_black_one.png" alt="black arrow here" width={30} height={30} />
+          </Link>
+          <Image className="bharat_chauhan_img" src="/images/bharat_chauhan_brown.jpg" alt="bharat chauhan photo here" width={500} height={500} />
+          <hr className="line" />
+
+          <div className="project-wrapper">
+            <p className="music">
+              <Link href="https://www.youtube.com/watch?v=3KY8jwrQ6vM" target="_blank">One Stream</Link>
+            </p>
+            <div className="project-link-wrapper">
+              <Link href="https://github.com/username/OneStream" target="_blank">
+                <i className="fab fa-github"></i> GitHub
+              </Link>
+              <Link href="your-target-page.html">
+                <div className="arrow-btn">
+                  <p>ᐅ</p>
+                </div>
+              </Link>
+            </div>
+          </div>
+          <hr className="line" />
+
+          <p className="music">
+            <Link href="https://www.youtube.com/watch?v=ENVW3uZ3a-4" target="_blank">Unihub</Link>
+            <span> | <Link href="https://github.com/username/Unihub" target="_blank"><i className="fab fa-github"></i> GitHub</Link></span>
+          </p>
+          <hr className="line" />
+
+          <p className="music">
+            <Link href="https://www.youtube.com/watch?v=PfHHm6k2Bv4" target="_blank">CodeNote</Link>
+            <span> | <Link href="https://github.com/username/CodeNote" target="_blank"><i className="fab fa-github"></i> GitHub</Link></span>
+          </p>
         </div>
 
-        {/* Bottom Sections */}
-        <div className="col-span-4 row-span-3 bg-gridBlocksBackground p-4 rounded-lg shadow-md">
-          <BottomLeftSection />
+        <div className="item item4">
+          <Image src="/images/another_circle_one.png" alt="another circle here" width={500} height={500} />
+          <p>
+            I am a dedicated web developer currently pursuing a B.Tech in Computer
+            Science, certified as an AWS Cloud Practitioner. My experience
+            includes optimizing web performance, implementing RESTful APIs, and
+            delivering cloud solutions for various projects. I am passionate about
+            continuous learning and apply creative solutions to complex problems.
+          </p>
         </div>
-        <div className="col-span-4 row-span-3 bg-[#3E3D38] p-4 rounded-lg shadow-md">
-          <BottomRightSection />
+
+        <div className="item item5">
+          <Image src="/images/arrow_white_one.png" alt="arrow image" width={30} height={30} />
+          <p className="questions">Have some <br /> Questions?</p>
+          <p className="contact_me">Contact <span>me</span></p>
         </div>
-        <div className="col-span-4 row-span-1 bg-gridBlocksBackground p-4 rounded-lg shadow-md">
-          <SocialsSection />
+
+        <div className="item item6">
+          <p><Link href="https://x.com/dubeyrishabh108" target="_blank">X.com</Link></p>
+          <p><Link href="https://www.linkedin.com/in/rishabh108/" target="_blank">LINKEDIN</Link></p>
+          <p><Link href="https://github.com/rishabh3562" target="_blank">GITHUB</Link></p>
         </div>
       </div>
-    </div>
+
+      {/* JS for navbar hamburger (optional, consider using React state) */}
+      <script src="https://kit.fontawesome.com/ae61999827.js" crossOrigin="anonymous"></script>
+      <script src="/index.js"></script>
+    </>
   );
 };
 
-export default Page;
+export default MyComponent;
--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <script src="https://kit.fontawesome.com/ae61999827.js" crossorigin="anonymous"></script>
       </body>
     </html>
   );
