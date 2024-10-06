b5e9147141afdcfc4051d86b013a116d52e19d31
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2024-10-06 08:04:51 +0530

    UI improvements

--- portfolio/src/app/home/page.jsx
+++ portfolio/src/app/home/page.jsx
@@ -1,6 +1,6 @@
 // components/MyComponent.js
-
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
@@ -21,13 +21,67 @@ import {
   FaBars,
 } from "react-icons/fa";
 
+export const jokes = [
+  {
+    question: `How many programmers does it take to change a light bulb?`,
+    answer: `None. It’s a "hardware" problem!`,
+  },
+  {
+    question: `Why do programmers hate nature?`,
+    answer: `Because it has too many "bugs"!`,
+  },
+  {
+    question: `Why did the programmer go broke?`,
+    answer: `Because he lost his "domain" in a bet!`,
+  },
+  {
+    question: `Why did the developer refuse to leave the house?`,
+    answer: `He was stuck in an "infinite loop"!`,
+  },
+  {
+    question: `Why did the developer go broke?`,
+    answer: `Because he used up all his "cache"!`,
+  },
+  {
+    question: `How did the developer get kicked out of the bar?`,
+    answer: `Because he kept trying to "commit" after every drink!`,
+  },
+  {
+    question: `Why did the SQL query break up with the NoSQL database?`,
+    answer: `It couldn't "join" the conversation!`,
+  },
+  {
+    question: `Why did the web developer break up with his girlfriend?`,
+    answer: `she had too many "dependencies" !`,
+  },
+  {
+    question: `What do you call a group of eight hobbits?`,
+    answer: `A hobbyte!`,
+  },
+  {
+    question: `Why did the computer keep freezing?`,
+    answer: `Because it left its "Windows" open!`,
+  },
+];
 const MyComponent = () => {
+  const [jokeIndex, setJokeIndex] = React.useState(0);
+  const handleNextJoke = () => {
+    window.onload = () => {
+      setJokeIndex((jokeIndex + 1) % jokes.length);
+    };
+  };
+  const [jokemessage, setJokeMessage] = useState(
+    jokes[jokeIndex].question ? jokes[jokeIndex].question : jokes[0].question
+  );
+  const [jokeanswer, setJokeAnswer] = useState(
+    jokes[jokeIndex].answer ? jokes[jokeIndex].answer : jokes[0].answer
+  );
   return (
     <>
       <nav>
         <ul className="nav-bar">
           <li className="logo">
-            <Link href="#">
+            <Link href="/home">
               Rishabh<span>Dubey</span>
             </Link>
           </li>
@@ -57,10 +111,13 @@ const MyComponent = () => {
           <Image
             src={circleImage}
             alt="circle image here"
-            width={500}
-            height={500}
+            width={110}
+            height={110}
           />
-          <p className="main_slogan">
+          <p
+            className="main_slogan
+           "
+          >
             AWS Certified <span> Cloud </span>Practitioner and
             <span>web </span> developer, crafting scalable web solutions that
             optimize performance and user experience.
@@ -71,7 +128,7 @@ const MyComponent = () => {
           <div className="card" id="jokeCard">
             <div className="card-inner">
               <div className="card-front">
-                <p id="jokeQuestion">Click to reveal the joke!</p>
+                <p id="jokeQuestion">{jokemessage}</p>
                 <p id="reveal" className="phone-block">
                   Hover/Tap to reveal
                 </p>
@@ -80,7 +137,7 @@ const MyComponent = () => {
                 </p>
               </div>
               <div className="card-back">
-                <p id="jokeAnswer"></p>
+                <p id="jokeAnswer">{jokeanswer}</p>
               </div>
             </div>
           </div>
@@ -169,8 +226,9 @@ const MyComponent = () => {
           <Image
             src={anotherCircleImage}
             alt="another circle here"
-            width={500}
-            height={500}
+            className="float-end"
+            width={50}
+            height={50}
           />
           <p>
             I am a dedicated web developer currently pursuing a B.Tech in
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -6,62 +6,70 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    // extend: {
-    //   colors: {
-    //     background: "var(--background)",
-    //     foreground: "var(--foreground)",
-    //     gridBackground: "var(--grid-background)",
-    //     gridBlocksBackground: "#D0BCA0",
-    //     gridBlocksBackground2: "#e6ddd1",
-    //     textDarkYellow: "var(--text-dark-yellow)",
-    //     textLightYellow: "var(--text-light-yellow)",
-    //     textDarkRed: "var(--text-dark-red)",
-    //     textSecondary: "var(--text-secondary)",
-    //     textTertiary: "var(--text-tertiary)",
-    //     primary: "var(--primary)",
-    //     secondary: "var(--secondary)",
-    //     accent: "var(--accent)",
-    //     font: "var(--font)",
-    //     lightBackground: "var(--lightBackground)",
-    //     lightText: "var(--lightText)",
-    //     darkText: "var(--darkText)",
-    //     primaryBackground: "var(--primaryBackground)",
-    //     secondaryBackground: "var(--secondaryBackground)",
-    //     // New custom colors
-    //     slateDark: "#121212",      // Dark background
-    //     slateLight: "#1c1c1c",     // Lighter background
-    //     beige: "#e6ddd1",          // Beige text background
-    //     blueBorder: "#4A90E2",     // Blue border color
-    //     greenBorder: "#2ECC71",    // Green border color
-    //     redBorder: "#E74C3C",      // Red border color
-    //   },
-    //   keyframes: {
-    //     fadeIn: {
-    //       '0%': { opacity: '0' },
-    //       '100%': { opacity: '1' },
-    //     },
-    //     wiggle: {
-    //       '0%, 100%': { transform: 'rotate(-3deg)' },
-    //       '50%': { transform: 'rotate(3deg)' },
-    //     },
-    //   },
-    //   animation: {
-    //     fadeIn: 'fadeIn 2s ease-in-out',
-    //     wiggle: 'wiggle 1s ease-in-out infinite',
-    //   },
-    //   gridTemplateRows: {
-    //     '8': 'repeat(8, minmax(0, 1fr))'
-    //   },
-    //   gridTemplateColumns: {
-    //     '12': 'repeat(12, minmax(0, 1fr))'
-    //   },
-    //   fontFamily: {
-    //     breathney: ['Breathney'],
-    //   },
-    //   screens: {
-    //     'xs': '320px'
-    //   }
-    // },
+    extend: {
+      colors: {
+        primaryColor: '#d0bca0',
+        bgColor: '#3e3d38',
+        lightDark: '#3c3d37',
+        darkDark: '#1e201e',
+      },
+    },
   },
   plugins: [],
 };
+// extend: {
+//   colors: {
+//     background: "var(--background)",
+//     foreground: "var(--foreground)",
+//     gridBackground: "var(--grid-background)",
+//     gridBlocksBackground: "#D0BCA0",
+//     gridBlocksBackground2: "#e6ddd1",
+//     textDarkYellow: "var(--text-dark-yellow)",
+//     textLightYellow: "var(--text-light-yellow)",
+//     textDarkRed: "var(--text-dark-red)",
+//     textSecondary: "var(--text-secondary)",
+//     textTertiary: "var(--text-tertiary)",
+//     primary: "var(--primary)",
+//     secondary: "var(--secondary)",
+//     accent: "var(--accent)",
+//     font: "var(--font)",
+//     lightBackground: "var(--lightBackground)",
+//     lightText: "var(--lightText)",
+//     darkText: "var(--darkText)",
+//     primaryBackground: "var(--primaryBackground)",
+//     secondaryBackground: "var(--secondaryBackground)",
+//     // New custom colors
+//     slateDark: "#121212",      // Dark background
+//     slateLight: "#1c1c1c",     // Lighter background
+//     beige: "#e6ddd1",          // Beige text background
+//     blueBorder: "#4A90E2",     // Blue border color
+//     greenBorder: "#2ECC71",    // Green border color
+//     redBorder: "#E74C3C",      // Red border color
+//   },
+//   keyframes: {
+//     fadeIn: {
+//       '0%': { opacity: '0' },
+//       '100%': { opacity: '1' },
+//     },
+//     wiggle: {
+//       '0%, 100%': { transform: 'rotate(-3deg)' },
+//       '50%': { transform: 'rotate(3deg)' },
+//     },
+//   },
+//   animation: {
+//     fadeIn: 'fadeIn 2s ease-in-out',
+//     wiggle: 'wiggle 1s ease-in-out infinite',
+//   },
+//   gridTemplateRows: {
+//     '8': 'repeat(8, minmax(0, 1fr))'
+//   },
+//   gridTemplateColumns: {
+//     '12': 'repeat(12, minmax(0, 1fr))'
+//   },
+//   fontFamily: {
+//     breathney: ['Breathney'],
+//   },
+//   screens: {
+//     'xs': '320px'
+//   }
+// },
