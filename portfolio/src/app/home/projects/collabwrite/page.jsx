f220a276a48e87c7a9f64ac339a6218905126450
rishabh3562 <rishabh.dubey.cs21@ggits.net>
2024-10-07 14:15:03 +0530

    UI changes and logic changes in urldata

--- portfolio/src/app/home/projects/collabwrite/page.jsx
+++ portfolio/src/app/home/projects/collabwrite/page.jsx
@@ -1,45 +1,13 @@
 // pages/index.js
 import React from 'react';
 import ProjectCard from '@/components/v1/ProjectStrcuture';
-import {oneStream} from '@/app/home/projects/data'
+import {CollabWrite,urldata} from '@/app/home/projects/data'
 const collabwrite = () => {
-  const project = {
-    title: "CollabWrite",
-    introduction: "CollabWrite is my and Divyesh Jain's submission for the Appwrite Hackathon.",
-    description: "Imagine having the freedom to brainstorm ideas, map out strategies, or present complex information in a visually captivating manner.",
-    techStack: [
-        { name: "NextJS", link: "https://nextjs.org/" },
-        { name: "TailwindCSS", link: "https://tailwindcss.com/" },
-        { name: "Appwrite", link: "https://appwrite.io/" },
-        { name: "Vercel", link: "https://vercel.com/dashboard" },
-    ],
-    features: [
-        "Use the canvas to draw to your heart's content. Create plans, diagrams, flowcharts, etc.",
-        "Go solo or collab with people to work together.",
-        "Real-time editing for collaborative efforts.",
-        "Inbuilt chat system for seamless communication.",
-        "Download your canvas for offline use.",
-    ],
-    flowImage: "https://cdn.hashnode.com/res/hashnode/image/upload/v1686607944692/e8a5602c-6b6a-46b8-b884-5a4fbfc71042.jpeg?auto=compress,format&format=webp",
-    signupLink: "https://collaboration-tool.vercel.app/login",
-    appwriteIntegration: "Connecting authentication with Appwrite was super easy. All project data, chat data, and invitation data are stored in the Appwrite database.",
-    challenges: [
-        "Working with the HTML Canvas API was not an easy task.",
-        "Drawing complex shapes was a challenge due to tracing limitations.",
-        "Implementing touch events for mobile compatibility required significant changes.",
-        "Unexpected mouse behaviors needed careful handling to optimize performance.",
-        "Deciding on data storage format was tricky; we settled on data:image format.",
-    ],
-    githubRepo: "https://github.com/heyyakash/CollaborationTool",
-    websiteLink: "https://collaboration-tool.vercel.app/",
-    videoDemo: "https://www.youtube.com/watch?v=1AIm2VsOQE4",
-    conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
-};
 
 
   return (
     <div className="flex flex-col items-center">
-      <ProjectCard project={oneStream} />
+      <ProjectCard project={CollabWrite} urlData={urldata.collabwrite} />
     </div>
   );
 };
--- portfolio/src/app/home/projects/data.js
+++ portfolio/src/app/home/projects/data.js
@@ -26,7 +26,8 @@ export const urldata = {
         postLink: "https://rishabhdubey108.vercel.app/home/projects/unihub",
         shareText:mainShrartext,
         githubUrl: "https://github.com/rishabhdubey108",
-        livelink: 'https://collaboration-tool.vercel.app/'
+        livelink: 'https://collaboration-tool.vercel.app/',
+        videolink:  "https://www.youtube.com/watch?v=1AIm2VsOQE4"
     }
 }
 
@@ -37,9 +38,9 @@ export const oneStream = {
     tags: ['CollabWrite', 'NextJS', 'TailwindCSS', 'Appwrite', 'Vercel'],
     links: {
         appwrite: "https://appwrite.io/",
-        github: "https://github.com/heyyakash/CollaborationTool",
-        website: "https://collaboration-tool.vercel.app/",
-        video: "https://www.youtube.com/watch?v=1AIm2VsOQE4"
+        github: urldata.oneStream.githubUrl?urldata.oneStream.githubUrl:null,
+        website:urldata.oneStream.livelink?urldata.oneStream.livelink:null,
+        video: urldata.oneStream.videolink?urldata.oneStream.videolink:null
     },
     techStack: [
         { name: "NextJS", link: "https://nextjs.org/" },
@@ -64,9 +65,10 @@ export const oneStream = {
         "Unexpected mouse behaviors needed careful handling to optimize performance.",
         "Deciding on data storage format was tricky; we settled on data:image format.",
     ],
-    githubRepo: "https://github.com/heyyakash/CollaborationTool",
-    websiteLink: "https://collaboration-tool.vercel.app/",
-    videoDemo: "https://www.youtube.com/watch?v=1AIm2VsOQE4",
+   
+    githubRepo:  urldata.oneStream.githubUrl?urldata.oneStream.githubUrl:null,
+    websiteLink: urldata.oneStream.livelink?urldata.oneStream.livelink:null,
+    videoDemo: urldata.oneStream.videolink?urldata.oneStream.videolink:null,
     conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
 };
 
@@ -75,84 +77,14 @@ export const oneStream = {
 
 export const unihub = {
     title: "CollabWrite",
-    introduction: "CollabWrite is my and Divyesh Jain's submission for the Appwrite Hackathon.",
-    description: "Imagine having the freedom to brainstorm ideas, map out strategies, or present complex information in a visually captivating manner.",
-    techStack: [
-        { name: "NextJS", link: "https://nextjs.org/" },
-        { name: "TailwindCSS", link: "https://tailwindcss.com/" },
-        { name: "Appwrite", link: "https://appwrite.io/" },
-        { name: "Vercel", link: "https://vercel.com/dashboard" },
-    ],
-    features: [
-        "Use the canvas to draw to your heart's content. Create plans, diagrams, flowcharts, etc.",
-        "Go solo or collab with people to work together.",
-        "Real-time editing for collaborative efforts.",
-        "Inbuilt chat system for seamless communication.",
-        "Download your canvas for offline use.",
-    ],
-    flowImage: "https://cdn.hashnode.com/res/hashnode/image/upload/v1686607944692/e8a5602c-6b6a-46b8-b884-5a4fbfc71042.jpeg?auto=compress,format&format=webp",
-    signupLink: "https://collaboration-tool.vercel.app/login",
-    appwriteIntegration: "Connecting authentication with Appwrite was super easy. All project data, chat data, and invitation data are stored in the Appwrite database.",
-    challenges: [
-        "Working with the HTML Canvas API was not an easy task.",
-        "Drawing complex shapes was a challenge due to tracing limitations.",
-        "Implementing touch events for mobile compatibility required significant changes.",
-        "Unexpected mouse behaviors needed careful handling to optimize performance.",
-        "Deciding on data storage format was tricky; we settled on data:image format.",
-    ],
-    githubRepo: "https://github.com/heyyakash/CollaborationTool",
-    websiteLink: "https://collaboration-tool.vercel.app/",
-    videoDemo: "https://www.youtube.com/watch?v=1AIm2VsOQE4",
-    conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
-};
-
-
-
-export const CodeNote = {
-    title: "CollabWrite",
-    introduction: "CollabWrite is my and Divyesh Jain's submission for the Appwrite Hackathon.",
-    description: "Imagine having the freedom to brainstorm ideas, map out strategies, or present complex information in a visually captivating manner.",
-    techStack: [
-        { name: "NextJS", link: "https://nextjs.org/" },
-        { name: "TailwindCSS", link: "https://tailwindcss.com/" },
-        { name: "Appwrite", link: "https://appwrite.io/" },
-        { name: "Vercel", link: "https://vercel.com/dashboard" },
-    ],
-    features: [
-        "Use the canvas to draw to your heart's content. Create plans, diagrams, flowcharts, etc.",
-        "Go solo or collab with people to work together.",
-        "Real-time editing for collaborative efforts.",
-        "Inbuilt chat system for seamless communication.",
-        "Download your canvas for offline use.",
-    ],
-    flowImage: "https://cdn.hashnode.com/res/hashnode/image/upload/v1686607944692/e8a5602c-6b6a-46b8-b884-5a4fbfc71042.jpeg?auto=compress,format&format=webp",
-    signupLink: "https://collaboration-tool.vercel.app/login",
-    appwriteIntegration: "Connecting authentication with Appwrite was super easy. All project data, chat data, and invitation data are stored in the Appwrite database.",
-    challenges: [
-        "Working with the HTML Canvas API was not an easy task.",
-        "Drawing complex shapes was a challenge due to tracing limitations.",
-        "Implementing touch events for mobile compatibility required significant changes.",
-        "Unexpected mouse behaviors needed careful handling to optimize performance.",
-        "Deciding on data storage format was tricky; we settled on data:image format.",
-    ],
-    githubRepo: "https://github.com/heyyakash/CollaborationTool",
-    websiteLink: "https://collaboration-tool.vercel.app/",
-    videoDemo: "https://www.youtube.com/watch?v=1AIm2VsOQE4",
-    conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
-};
-
-
-
-export const CollabWrite = {
-    title: "CollabWrite",
     introduction: "CollabWrite",
     description: "Imagine having the freedom to brainstorm ideas, map out strategies, or present complex information in a visually captivating manner.",
     tags: ['CollabWrite', 'NextJS', 'TailwindCSS', 'Appwrite', 'Vercel'],
     links: {
         appwrite: "https://appwrite.io/",
-        github: "https://github.com/heyyakash/CollaborationTool",
-        website: "https://collaboration-tool.vercel.app/",
-        video: "https://www.youtube.com/watch?v=1AIm2VsOQE4"
+        github: urldata.oneStream.githubUrl?urldata.oneStream.githubUrl:null,
+        website:urldata.oneStream.livelink?urldata.oneStream.livelink:null,
+        video: urldata.oneStream.videolink?urldata.oneStream.videolink:null
     },
     techStack: [
         { name: "NextJS", link: "https://nextjs.org/" },
@@ -177,8 +109,94 @@ export const CollabWrite = {
         "Unexpected mouse behaviors needed careful handling to optimize performance.",
         "Deciding on data storage format was tricky; we settled on data:image format.",
     ],
-    githubRepo: "https://github.com/heyyakash/CollaborationTool",
-    websiteLink: "https://collaboration-tool.vercel.app/",
-    videoDemo: "https://www.youtube.com/watch?v=1AIm2VsOQE4",
+   
+    githubRepo:  urldata.oneStream.githubUrl?urldata.oneStream.githubUrl:null,
+    websiteLink: urldata.oneStream.livelink?urldata.oneStream.livelink:null,
+    videoDemo: urldata.oneStream.videolink?urldata.oneStream.videolink:null,
+    conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
+};
+
+
+
+export const CodeNote = {
+    title: "CollabWrite",
+    introduction: "CollabWrite",
+    description: "Imagine having the freedom to brainstorm ideas, map out strategies, or present complex information in a visually captivating manner.",
+    tags: ['CollabWrite', 'NextJS', 'TailwindCSS', 'Appwrite', 'Vercel'],
+    links: {
+        appwrite: "https://appwrite.io/",
+        github: urldata.oneStream.githubUrl?urldata.oneStream.githubUrl:null,
+        website:urldata.oneStream.livelink?urldata.oneStream.livelink:null,
+        video: urldata.oneStream.videolink?urldata.oneStream.videolink:null
+    },
+    techStack: [
+        { name: "NextJS", link: "https://nextjs.org/" },
+        { name: "TailwindCSS", link: "https://tailwindcss.com/" },
+        { name: "Appwrite", link: "https://appwrite.io/" },
+        { name: "Vercel", link: "https://vercel.com/dashboard" },
+    ],
+    features: [
+        "Use the canvas to draw to your heart's content. Create plans, diagrams, flowcharts, etc.",
+        "Go solo or collab with people to work together.",
+        "Real-time editing for collaborative efforts.",
+        "Inbuilt chat system for seamless communication.",
+        "Download your canvas for offline use.",
+    ],
+    flowImage: "https://cdn.hashnode.com/res/hashnode/image/upload/v1686607944692/e8a5602c-6b6a-46b8-b884-5a4fbfc71042.jpeg?auto=compress,format&format=webp",
+    signupLink: "https://collaboration-tool.vercel.app/login",
+    appwriteIntegration: "Connecting authentication with Appwrite was super easy. All project data, chat data, and invitation data are stored in the Appwrite database.",
+    challenges: [
+        "Working with the HTML Canvas API was not an easy task.",
+        "Drawing complex shapes was a challenge due to tracing limitations.",
+        "Implementing touch events for mobile compatibility required significant changes.",
+        "Unexpected mouse behaviors needed careful handling to optimize performance.",
+        "Deciding on data storage format was tricky; we settled on data:image format.",
+    ],
+   
+    githubRepo:  urldata.oneStream.githubUrl?urldata.oneStream.githubUrl:null,
+    websiteLink: urldata.oneStream.livelink?urldata.oneStream.livelink:null,
+    videoDemo: urldata.oneStream.videolink?urldata.oneStream.videolink:null,
+    conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
+};
+
+
+export const CollabWrite ={
+    title: "CollabWrite",
+    introduction: "CollabWrite",
+    description: "Imagine having the freedom to brainstorm ideas, map out strategies, or present complex information in a visually captivating manner.",
+    tags: ['CollabWrite', 'NextJS', 'TailwindCSS', 'Appwrite', 'Vercel'],
+    links: {
+        appwrite: "https://appwrite.io/",
+        github: urldata.collabwrite.githubUrl?urldata.collabwrite.githubUrl:null,
+        website:urldata.collabwrite.livelink?urldata.collabwrite.livelink:null,
+        video: urldata.collabwrite.videolink?urldata.collabwrite.videolink:null
+    },
+    techStack: [
+        { name: "NextJS", link: "https://nextjs.org/" },
+        { name: "TailwindCSS", link: "https://tailwindcss.com/" },
+        { name: "Appwrite", link: "https://appwrite.io/" },
+        { name: "Vercel", link: "https://vercel.com/dashboard" },
+    ],
+    features: [
+        "Use the canvas to draw to your heart's content. Create plans, diagrams, flowcharts, etc.",
+        "Go solo or collab with people to work together.",
+        "Real-time editing for collaborative efforts.",
+        "Inbuilt chat system for seamless communication.",
+        "Download your canvas for offline use.",
+    ],
+    flowImage: "https://cdn.hashnode.com/res/hashnode/image/upload/v1686607944692/e8a5602c-6b6a-46b8-b884-5a4fbfc71042.jpeg?auto=compress,format&format=webp",
+    signupLink: "https://collaboration-tool.vercel.app/login",
+    appwriteIntegration: "Connecting authentication with Appwrite was super easy. All project data, chat data, and invitation data are stored in the Appwrite database.",
+    challenges: [
+        "Working with the HTML Canvas API was not an easy task.",
+        "Drawing complex shapes was a challenge due to tracing limitations.",
+        "Implementing touch events for mobile compatibility required significant changes.",
+        "Unexpected mouse behaviors needed careful handling to optimize performance.",
+        "Deciding on data storage format was tricky; we settled on data:image format.",
+    ],
+   
+    githubRepo:  urldata.collabwrite.githubUrl?urldata.collabwrite.githubUrl:null,
+    websiteLink: urldata.collabwrite.livelink?urldata.collabwrite.livelink:null,
+    videoDemo: urldata.collabwrite.videolink?urldata.collabwrite.videolink:null,
     conclusion: "Appwrite played a pivotal role in bringing our project to life. We are committed to further enhancing this project."
 };
